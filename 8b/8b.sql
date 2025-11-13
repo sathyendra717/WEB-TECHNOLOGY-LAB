953d17a0a91fafa8a7e1cd7341e0b0985cdfc511
sathyendra717 <sathyendra.23cs140@sode-edu.in>
2025-11-13 10:49:30 +0530

    Create SQL script for students database and table

---
+++ 8b/8b.sql
@@ -0,0 +1,9 @@
+show databases;
+
+create database students124;
+use students124;
+create table students124 (id int primary key , name varchar(50) , grade float);
+
+insert into students124 values (1,"Tharun",99);
+insert into students124 values (2,"Vinayak",90);
+insert into students124 values (3,"Swaroop",95);
